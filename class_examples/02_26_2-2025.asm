e187efc174baba806060a81ec23b6354fa907266
michaelfournier064 <michaelfournier064@outlook.com>
2025-02-26 11:56:36 -0500

    feat(class_examples): Code notes for 02/26/2025 on how to loop through a string and a DWORD

--- class_examples/02_26_2-2025.asm
+++ class_examples/02_26_2-2025.asm
@@ -1,6 +1,4 @@
-
-
-;Beginning Lab
+;Class Examples
 ;COSC 350
 ;Michael Fournier
 
@@ -9,12 +7,39 @@
 .stack 4096
 ExitProcess proto,dwExitCode:dword
 .data
-
+	;declare variables here
+	myarr	BYTE	11h, 22h, 33h, 44h
+	mydwarr	DWORD	1a1b1c1dh, 2a2b2c2dh, 3a3b3c3dh, 4a4b4c4dh
+	mystr	BYTE	"Hello, World!", 0dh, 0ah, 0
 
 .code
 main proc
+	; Basic information about array sizes
+	mov eax,	LENGTHOF	myarr   ; Number of elements in myarr
+	mov ebx,	TYPE		mydwarr ; Type size (DWORD = 4 bytes)
+	mov ecx,	LENGTHOF	mydwarr ; Number of DWORDs
+	mov edx,	SIZEOF		mydwarr ; Total byte size
 
+	; Print characters from mystr one by one
+	mov ecx, LENGTHOF mystr
+	mov esi, OFFSET mystr
 
-	invoke ExitProcess,0
+top:
+	mov al, [esi]   ; Load a single character into AL
+	inc esi         ; Move to next character
+
+	loop top        ; Loop until ECX reaches 0
+
+	; Loop through DWORD array
+	mov ecx, LENGTHOF mydwarr
+	mov esi, OFFSET mydwarr
+
+topDw:
+	mov eax, [esi]  ; Load a DWORD into EAX
+	add esi, 4      ; Move to next DWORD
+
+	loop topDw      ; Loop until ECX reaches 0
+
+	invoke ExitProcess, 0
 main endp
 end main
