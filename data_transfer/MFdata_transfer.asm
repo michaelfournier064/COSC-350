369bafbadbb31f9b63e0b3052431c8b460d93287
michaelfournier064 <michaelfournier064@outlook.com>
2025-02-26 10:29:02 -0500

    feat(data_transfer): Exercise One for data transfer lab completed

--- data_transfer/MFdata_transfer.asm
+++ data_transfer/MFdata_transfer.asm
@@ -6,12 +6,30 @@
 .model flat,stdcall
 .stack 4096
 ExitProcess proto,dwExitCode:dword
-.data
 
+.data
+	;Exercise One--Creating definition's and initialize values 
+	;for the following data types BYTE, SBYTE, WORD, SWORD, DWORD, SDWORD, REAL4
+	myBYTE		BYTE		22h
+	mySBYTE		SBYTE		-22h
+	myWORD		WORD		4444h
+	mySWORD		SWORD		-4444h
+	myDWORD		DWORD		88888888h
+	mySDWORD	SDWORD		-88888888h
+	myREAL4		REAL4		3.1415927
 
 .code
 main proc
-
+	;Exercise One--Moving variables into a register that properly contains their value
+	;for the following data types BYTE, SBYTE, WORD, SWORD, DWORD, SDWORD, REAL4
+	mov ah,	myBYTE
+	mov	al,	mySBYTE
+	mov bx, myWORD
+	shl ebx, 16    ; Shift BX value into the upper half of EBX
+	mov bx, mySWORD
+	mov eax, myDWORD
+	mov edx, mySDWORD
+	fld myREAL4    ; Load floating-point value into FPU
 
 	invoke ExitProcess,0
 main endp
