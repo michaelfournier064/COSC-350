3bfc2978b4b7c8213238c5383c9fe30c0d75fc55
Michael Fournier <michaelfournier064@outlook.com>
2025-03-07 22:49:04 -0500

    feat(textbook_examples): Assembly code that takes a signed 32-bit int and outputs its decimal, hex, and binary form.

--- textbook_examples/Tutorial Library Test #1.asm
+++ textbook_examples/Tutorial Library Test #1.asm
@@ -1,5 +1,4 @@
 ;*****************************************************************
-TITLE Library Test #1: Integer I/O (InputLoop.asm)
 ; Tests the Clrscr, Crlf, DumpMem, ReadInt, SetTextColor, 
 ; WaitMsg, WriteBin, WriteHex, and WriteString procedures.
 ; Course:     COSC 350
@@ -23,6 +22,9 @@ INCLUDE     Irvine32.inc
 
 .code
 main PROC
+;*****************************************************************
+; Part 1--Changing the text and background color, 
+;*****************************************************************
     mov eax, BlueTextOnGray     ; Moves the information from memory to register
     call SetTextColor           ; Changes the stored color values
 
@@ -35,7 +37,34 @@ main PROC
     mov ecx, LENGTHOF arrayD    ; Number of SDWORD's in arrayD
     call DumpMem                ; Displays the memory informaion
 
+;*****************************************************************
+; Part 2
+;*****************************************************************
+    call Crlf                   ; Outputs a empty line by terminating the previous one
+    mov ecx, COUNT              ; Copies COUNT into the ecx register
 
+    L1:
+        mov edx, OFFSET prompt
+        call WriteString
+        call ReadInt            ; Input 32-bit signed integer into eax from the keyboard
+        call Crlf               ; Displays a new line by terminating the previous one
+        
+        call WriteInt           ; Outputs eax in signed decimal format
+        call Crlf               ; Displays a new line by terminating the previous one
+
+        call WriteHex           ; Outputs eax in hexidecimal
+        call Crlf               ; Displays a new line by terminating the previous one
+        call WriteBin           ; Outputs eax in binary
+        call Crlf               ; Displays a new line by terminating the previous one
+        call Crlf               ; Displays a new line by terminating the previous one
+
+        Loop L1                 ; Number of loops is set by ecx, which is set to COUNT outside of the loop
+
+    call WaitMsg            ; Outputs a message promtting the user to input a key for the program to contine
+
+    mov eax, DefaultColor
+    call SetTextColor
+    call Clrscr
 
 
     invoke ExitProcess, 0
